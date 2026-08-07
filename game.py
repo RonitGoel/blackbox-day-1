cf4793ab033c3ca4615fe1ebd3d5df9505af278f
Ronit Goel <goelronit03@gmail.com>
2026-08-07 17:24:10 +0530

    Refactor guess the number game logic

--- game.py
+++ game.py
@@ -1,18 +1,20 @@
 # This a guess the number game 
 # You have 10 tries
 import random
-x=random.randint(1,100)
-y=0
-while y <10 :
+secret_number=random.randint(1,100)
+attempts=0
+while attempts <10 :
     guess = int(input("Whats your guess ?"))
-    if x == guess:
+    if secret_number == guess:
             print("Great guess")
             break
-    elif guess >x:
+    elif guess >secret_number:
             print("Guess is higher than the number")
-    elif guess < x:
+    elif guess < secret_number:
             print("Guess is lower than the number")   
 
-    y = y+1
+    attempts = attempts+1
 else:
-       print("you are out of chances")  
+       print("You are out of chances")  
+
+
